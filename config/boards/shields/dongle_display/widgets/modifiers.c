492d932027abf9c40e75e2dd4780aa9c90325f4e
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2024-08-21 23:20:27 +0200

    urob mouse, oled changes

--- config/boards/shields/dongle_display/widgets/modifiers.c
+++ config/boards/shields/dongle_display/widgets/modifiers.c
@@ -68,9 +68,9 @@ struct modifier_symbol ms_cmd = {
 
 struct modifier_symbol *modifier_symbols[] = {
     // this order determines the order of the symbols
+    &ms_control,
     &ms_cmd,
     &ms_opt,
-    &ms_control,
     &ms_shift
 };
 
