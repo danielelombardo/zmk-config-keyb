36733a505ed4288c4ea36228a051494e2ba4af91
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2024-08-23 17:32:31 +0200

    change battery widget size, remove HID ind

--- config/boards/shields/dongle_display/widgets/battery_status.c
+++ config/boards/shields/dongle_display/widgets/battery_status.c
@@ -27,7 +27,7 @@ struct peripheral_battery_state {
     uint8_t level;
 };
     
-static lv_color_t battery_image_buffer[ZMK_SPLIT_BLE_PERIPHERAL_COUNT][5 * 8];
+static lv_color_t battery_image_buffer[ZMK_SPLIT_BLE_PERIPHERAL_COUNT][5 * 12];
 
 static void draw_battery(lv_obj_t *canvas, uint8_t level) {
     // inverted colors
