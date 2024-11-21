5241850c8720ddba24544837fce1fa2bf6ac21f9
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2024-11-21 15:28:50 +0100

    screen battery bars-colors

--- config/boards/shields/dongle_display/widgets/battery_status.c
+++ config/boards/shields/dongle_display/widgets/battery_status.c
@@ -27,7 +27,7 @@ struct peripheral_battery_state {
     uint8_t level;
 };
     
-static lv_color_t battery_image_buffer[ZMK_SPLIT_BLE_PERIPHERAL_COUNT][15 * 2];
+static lv_color_t battery_image_buffer[ZMK_SPLIT_BLE_PERIPHERAL_COUNT][15 * 3];
 
 static void draw_battery(lv_obj_t *canvas, uint8_t level) {
     // inverted colors
@@ -36,8 +36,8 @@ static void draw_battery(lv_obj_t *canvas, uint8_t level) {
     lv_draw_rect_dsc_t rect_fill_dsc;
     lv_draw_rect_dsc_init(&rect_fill_dsc);
     rect_fill_dsc.bg_color = lv_color_white();
-    uint8_t level_bar = 14 * level / 100;
-    lv_canvas_draw_rect(canvas, 0, 0, level_bar, 2, &rect_fill_dsc); 
+    uint8_t level_bar = 30 * (level / 100 - 1);
+    lv_canvas_draw_rect(canvas, 0, level_bar, 30, 2, &rect_fill_dsc); 
 }
 
 
@@ -84,7 +84,7 @@ int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_batte
         lv_obj_t *battery_label = lv_label_create(widget->obj);
         lv_obj_t *image_canvas = lv_canvas_create(widget->obj);
         lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, i * 105, 0);
-        lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 15, 2, LV_IMG_CF_TRUE_COLOR);
+        lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 30, 3, LV_IMG_CF_TRUE_COLOR);
         lv_obj_align(image_canvas, LV_ALIGN_BOTTOM_LEFT, i * 105, 0);
         lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
