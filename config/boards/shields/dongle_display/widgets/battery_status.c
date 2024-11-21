47d4e372427770209a464d120e2739c67db5ed99
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2024-11-21 14:30:31 +0100

    screen battery bars-colors

--- config/boards/shields/dongle_display/widgets/battery_status.c
+++ config/boards/shields/dongle_display/widgets/battery_status.c
@@ -83,9 +83,9 @@ int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_batte
     for (int i = 0; i < ZMK_SPLIT_BLE_PERIPHERAL_COUNT; i++) {
         lv_obj_t *battery_label = lv_label_create(widget->obj);
         lv_obj_t *image_canvas = lv_canvas_create(widget->obj);
-        lv_obj_align(battery_label, LV_ALIGN_IN_BOTTOM_LEFT, i * 105, 0);
+        lv_obj_align(battery_label, LV_ALIGN_BOTTOM_LEFT, i * 105, -2);
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 15, 2, LV_IMG_CF_TRUE_COLOR);
-        lv_obj_align(image_canvas, LV_ALIGN_IN_BOTTOM_LEFT, i * 105, 0);
+        lv_obj_align(image_canvas, LV_ALIGN_BOTTOM_LEFT, i * 105, 0);
         lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
     }
