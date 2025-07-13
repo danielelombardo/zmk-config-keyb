2a38bba410806784b6467987ccb093852cecfae8
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2025-07-13 22:18:25 +0200

    new display #4

--- config/boards/shields/display_adapter/src/widgets/battery_layer.c
+++ config/boards/shields/display_adapter/src/widgets/battery_layer.c
@@ -13,7 +13,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/event_manager.h>
 #include <zmk/keymap.h>
 
-#include "<battery_layer.h>"
+#include "battery_layer.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
